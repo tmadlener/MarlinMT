a221e21f281058f60a47667a24db148926452837
Julian Benda <bendajul@flc29.desy.de>
2019-10-11 17:56:15 +0200

    Added MemLayout Demo

--- test/tests/test-mem-layout.cc
+++ test/tests/test-mem-layout.cc
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <limits>
-#include <cmath>
 
 #include <UnitTesting.h>
 #include "marlin/book/MemLayout.h"
@@ -10,14 +8,19 @@
 #include "ROOT/RAxis.hxx"
 
 using namespace ROOT::Experimental;
-using namespace marlin::book;
+
 struct Type1 {
 	std::array<int, 5> bins;
-	Type1(int a0, int a1, int a2, int a3, int a4)
-		: bins{a0, a1, a2, a3, a4} {}
+	Type1(int a0, int a1, int a2, int a3, int a4) {
+		bins[0] = a0;
+		bins[1] = a1;
+		bins[2] = a2;
+		bins[3] = a3;
+		bins[4] = a4;
+	}
 };
 
-void MergeType1(const std::shared_ptr<Type1>& p1, const std::shared_ptr<Type1>& p2) {
+void MergeType1(std::shared_ptr<Type1>& p1, std::shared_ptr<Type1>& p2) {
 	for(int i = 0; i < 5; ++i) {
 		p1->bins[i] += p2->bins[i];
 	}
@@ -28,13 +31,13 @@ public:
 	SharedType1 (std::size_t num_inst, Args_t ... args) : SharedMemLayout<Type1, MergeType1, Args_t ... >{num_inst, args ...}{}
 };
 
-void MergeHist(const std::shared_ptr<RH1I>& p1, const std::shared_ptr<RH1I>& p2) {
+void MergeHist(std::shared_ptr<RH1D>& p1, std::shared_ptr<RH1D>& p2) {
 	ROOT::Experimental::Add(*p1, *p2);
 }
-
-class SharedRH1I : public SharedMemLayout<RH1I, MergeHist, RAxisConfig> {
+template<typename ... Args_t>
+class SharedRH1D : public SharedMemLayout<RH1D, MergeHist, Args_t ...> {
 public:
-	SharedRH1I (std::size_t num_inst, RAxisConfig config) : SharedMemLayout<RH1I, MergeHist, RAxisConfig>{num_inst, config} {}
+	SharedRH1D (std::size_t num_inst, Args_t ... args) : SharedMemLayout<RH1D, MergeHist, Args_t...>{num_inst, args ...} {}
 };
 
 int main (int, char**) {
@@ -43,33 +46,30 @@ int main (int, char**) {
 	SharedType1 sMem(3, 1, 0, 0, 0, 0);
 	auto ptr1 = sMem.at<Type1>(0);
 	auto ptr2 = sMem.at<Type1>(1);
+	
 	ptr1->bins[0] += 3;
 	ptr2->bins[0] += 2;
-	test.test("bin content test ", sMem.merged<Type1>()->bins[0] == 9);
+	
+	test.test("bin content test ", sMem.merged<Type1>()->bins[0] == 8);
 
-	SharedRH1I sMemH(3, {"x", 2, 0, RAND_MAX});
-	RH1D refHist({"x", 2, 0, RAND_MAX});
-	auto ptrH1 = sMemH.at<RH1I>(0);
-	auto ptrH2 = sMemH.at<RH1I>(1);
+	auto config = RAxisConfig("x", 2, 0, 1);
+	SharedRH1D sMemH(3, config);
+	RH1D refHist(config);
+	auto ptrH1 = sMemH.at<RH1D>(0);
+	auto ptrH2 = sMemH.at<RH1D>(1);
 
+	const double max = static_cast<double>(RAND_MAX);
 	for(int i = 0; i < 10; ++i) {
-		double x
-			= static_cast<double>(std::rand()) / static_cast<double>(RAND_MAX);
-		int w = std::rand() % 100;
-
+		double x = static_cast<double>(std::rand()) / max;
+		double w = static_cast<double>(std::rand()) / max;
 		if(std::rand() > RAND_MAX / 2) {
 			ptrH1->Fill({x}, w);
-			std::cout << ptrH1->GetBinContent({x});
 		} else {
 			ptrH2->Fill({x}, w);
-			std::cout << ptrH2->GetBinContent({x});
 		}
 		refHist.Fill({x}, w);
 	}
-
-	test.test("Test shared RH1D", 
-		refHist.GetBinContent({0}) == sMemH.merged<RH1I>()->GetBinContent({0})
-	) ;
+	test.test("Test shared RH1D", refHist.GetBinContent({0}) == sMemH.merged<RH1D>()->GetBinContent({0}));
 	return 0;
 }
 
