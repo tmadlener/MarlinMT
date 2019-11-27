34c49a57ef36c436f4a81dde8d8ee22ea0a5ed7a
Remi Ete <eteremi@flc03.desy.de>
2019-11-27 19:15:17 +0100

    Fixed main travis CI script

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-source setup.sh
+source $HOME/setup.sh
 
-cd Package
+cd /Package
 mkdir build
 cd build
 # TODO enable 'warning as error' asap
 cmake \
-  -C ../../CMakeCache.cmake \
+  -C $HOME/CMakeCache.cmake \
   -DCMAKE_INSTALL_PREFIX=$PWD/../install \
   -DCMAKE_CXX_STANDARD=17 \
   -DMARLIN_BOOK=ON \
