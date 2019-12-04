4d7039480b44707e6408276667a715024151f0de
Remi Ete <eteremi@flc03.desy.de>
2019-12-04 17:18:16 +0100

    Added new deploy_documentation.sh script for TravisCI

---
+++ .travis-ci.d/deploy_documentation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source $HOME/setup.sh
+
+cd /Package
+mkdir -p build
+cd build
+
+# Re-run the CMake command with MKDocs ON. 
+# This keeps the options from the previous build, 
+# so the doxygen sources will be populated according to the options 
+# switched ON before running this. 
+cmake \
+  -C $HOME/CMakeCache.cmake \
+  -DCMAKE_INSTALL_PREFIX=$PWD/../install \
+  -DMARLIN_MKDOCS=ON \
+  ..
+  
+# Run mkdocs
+make mkdocs
+
+# setup the remote with the access token
+cd ..
+git remote rm origin
+git remote add origin https://rete:$GITHUB_TOKEN@github.com/iLCSoft/MarlinMT.git
+
+# copy the site to the Package directory.
+# Can't relocate the site dir using gh-deploy 
+cp -r build/MkDocs/site .
+
+# Push the doc
+mkdocs gh-deploy --force
