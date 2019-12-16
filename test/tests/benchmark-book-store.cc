acebc3ab3200c9026f8f1bd8a60bbe3428f54e87
Julian Benda <julian.benda@ovgu.de>
2019-12-16 18:09:49 +0100

    Modified Benchmark to test
    * failed when performance lose is about 5%, may change later

--- test/tests/benchmark-book-store.cc
+++ test/tests/benchmark-book-store.cc
@@ -6,6 +6,8 @@
 
 #include "ROOT/RHist.hxx"
 
+#include <UnitTesting.h>
+
 #include "marlin/book/Handle.h"
 #include "marlin/book/BookStore.h"
 #include "marlin/book/Hist.h"
@@ -15,8 +17,11 @@ constexpr unsigned int NumThreads = 3;
 constexpr unsigned int nWrites = 1000000;
 constexpr std::pair<float, float> range(0, 1000);
 
-int main(int argc, char *argv[])
+int main(int /*argc*/, char * /*argv*/[])
 {
+
+  marlin::test::UnitTest test(" Performance Test Filling: ");
+
   marlin::book::BookStore store;
   marlin::book::Handle entry = store.book("/", "hist", marlin::book::EntryData<marlin::book::types::H1F>("title", {"a", 250, range.first, range.second}).single());
   marlin::book::Handle hist = entry.handle(0);
@@ -28,13 +33,15 @@ int main(int argc, char *argv[])
     std::mt19937 e2(rd());
     std::uniform_real_distribution<> dist(range.first, range.second);
     for(float& v : numbers) {
-      v = std::floor(dist(e2));
+      v = static_cast<float>(dist(e2));
     }
   }
   std::chrono::duration<double> bookStoreFill{};
   std::chrono::duration<double> rootFill{};
+  
+  std::size_t count1 = 0, count2 = 0;
 
-  for(unsigned int i = 0; i < 1; ++i) {
+  for(unsigned int i = 0; i < 1000; ++i) {
     auto start = std::chrono::high_resolution_clock::now();
     for(float v : numbers) {
       hist.fill({v}, 1);
@@ -49,9 +56,20 @@ int main(int argc, char *argv[])
     }
     end = std::chrono::high_resolution_clock::now();
     rootFill += end - start;
+
+    count1 += hist.merged().get().GetEntries();
+    count2 += rhist.GetEntries();
   }
-  std::cout << "hits: " << hist.merged().get().GetEntries() << " == " << rhist.GetEntries() << '\n';
+  std::cout << "hits: " << count1 << " == " << count2 << '\n';
   std::cout << "             \tStore\t\tRoot\n"
             << "Time to fill:\t"<<bookStoreFill.count() << "\t" << rootFill.count() << "\n";
+
+  double moreTime = (bookStoreFill.count() / rootFill.count()) - 1.0;
+  test.test(
+      std::string("performance lose from more then 5%! ")
+        + std::to_string(moreTime),
+        moreTime < 0.05
+      );  
+
   return 0;
 }
