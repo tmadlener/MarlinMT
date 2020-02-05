5367e5f65c57acba8c2aafcce3aaef7e871a4cd6
Julian Benda <julian.benda@ovgu.de>
2020-02-05 15:46:11 +0100

    added plot srcipt for new structor

---
+++ benchmarking/PlotScalingBookStore.C
@@ -0,0 +1,159 @@
+#include <vector>
+#include <string>
+#include <exception>
+#include <map>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <locale>
+
+
+struct Entry {
+  std::size_t     _ncores {0} ;
+  std::size_t     _crunchTime {0} ;
+  double          _crunchSigma {0.} ;
+	std::size_t 		_nHist {0} ;
+	std::size_t 		_nFills {0} ;
+	std::size_t 		_nBins {0} ;
+  double          _serialTime {0.} ;
+  double          _parallelTime {0.} ;
+  double          _speedup {0.} ;
+	friend std::istream& operator>>(std::istream& is, Entry& entry) {
+		char c;
+		is	
+			>> entry._ncores >> c 
+			>> entry._crunchTime  >> c
+			>> entry._crunchSigma  >> c
+			>> entry._nHist >> c
+			>> entry._nFills >> c
+			>> entry._nBins >> c
+			>> entry._serialTime >> c 
+			>> entry._parallelTime  >> c
+			>> entry._speedup ;
+		return is;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const Entry& entry) {
+		os 
+			<< entry._ncores << ", " 
+			<< entry._crunchTime << ", "
+			<< entry._crunchSigma << ", "
+			<< entry._nHist<< ", "
+			<< entry._nFills<< ", "
+			<< entry._nBins<< ", "
+			<< entry._serialTime << ", "
+			<< entry._parallelTime << ", "
+			<< entry._speedup ;
+		return os;
+	}
+};
+
+static const std::vector<int> plotColors = { kGray, kBlue, kRed, kViolet, kOrange, kGreen, kOrange, kAzure } ; 
+
+int getColor( int index ) {
+  int trueIndex = index % plotColors.size() ;
+  int colorShift = (index / plotColors.size() ) + 1 ;
+  return plotColors[ trueIndex ] + colorShift ;
+}
+
+
+void PlotScaling( const std::string &fname, const std::string &title = "", bool save = false ) {
+
+
+  std::ifstream file ( fname ) ;
+
+  if( not file ) {
+    throw std::runtime_error( "Input file is invalid" ) ;
+  }
+
+  // std::map<std::size_t, std::vector<Entry>>   coreToEntryMap ;
+	std::map<std::pair<std::size_t, std::size_t>, std::vector<Entry>>
+		crunchNHistToEntryMap {} ;
+	
+	Entry entry;
+	std::string header;
+	std::getline(file, header);
+  while( file >> entry ) {
+		crunchNHistToEntryMap[{entry._crunchTime, entry._nHist}].push_back(entry);
+  }
+	TCanvas *speedupVsCoreCanvas = 
+		new TCanvas( 
+			"speedupVsCoreCanvas", 
+			"Speedup Vs #Core", 800, 800 ) ;
+	speedupVsCoreCanvas->SetMargin( 0.130326, 0.0538847, 0.130491, 0.0917313 ) ;
+	for (int i = 0; i < 300; i+=100){ 
+		TMultiGraph *speedupVsCoreMultiGraph = new TMultiGraph() ;
+		
+		// loop over crunch times
+		int color = 0 ;
+		size_t maxCores = 0 ;
+		for( auto ctIter : crunchNHistToEntryMap ) {
+			
+			auto crunchTime = ctIter.first.first ;
+			if ( crunchTime != i ) { continue; }
+			auto nHist = ctIter.first.second;
+			std::stringstream title ; title << nHist << "  " << crunchTime ;
+			if( ctIter.second.at(0)._crunchSigma != 0. ) {
+				title << " #pm " << ctIter.second.at(0)._crunchSigma ;
+			}
+			title << " ms" ;
+
+			TGraph *crunchTimeGraph = new TGraph( ctIter.second.size() ) ;
+			crunchTimeGraph->SetName( title.str().c_str() ) ;
+			crunchTimeGraph->SetTitle( title.str().c_str() ) ;
+			speedupVsCoreMultiGraph->Add( crunchTimeGraph ) ;
+			crunchTimeGraph->SetLineWidth( 3 ) ;
+			crunchTimeGraph->SetLineColor( getColor(color) ) ;
+			crunchTimeGraph->SetMarkerStyle( 0 ) ;
+
+			// loop over ncores for a given crunch time
+			for( unsigned int i=0 ; i<ctIter.second.size() ; i++ ) {
+				auto &entry = ctIter.second.at( i ) ;
+				crunchTimeGraph->SetPoint( i, entry._ncores, entry._speedup ) ;
+				maxCores = std::max( maxCores, entry._ncores ) ;
+			}
+			color++ ;
+		}
+
+		speedupVsCoreCanvas->cd() ;
+		speedupVsCoreCanvas->SetGridx();
+		speedupVsCoreCanvas->SetGridy();  
+
+		if( not title.empty() ) {
+			speedupVsCoreMultiGraph->SetTitle( title.c_str() ) ;
+		}
+
+		speedupVsCoreMultiGraph->Draw( "alp" ) ;
+		speedupVsCoreMultiGraph->GetXaxis()->SetTitle( "# Cores" ) ;
+		speedupVsCoreMultiGraph->GetYaxis()->SetTitle( "Speedup" ) ;
+		speedupVsCoreMultiGraph->GetXaxis()->SetTitleSize( 0.05 ) ;
+		speedupVsCoreMultiGraph->GetYaxis()->SetTitleSize( 0.05 ) ;
+		speedupVsCoreMultiGraph->GetXaxis()->SetRangeUser( 0, maxCores+1 ) ;
+		speedupVsCoreMultiGraph->GetYaxis()->SetRangeUser( 0, maxCores+1 ) ;
+		
+		auto legend = speedupVsCoreCanvas->BuildLegend( 0.15, 0.47, 0.47, 0.89, "" ) ;
+		legend->SetTextSize( 0.035 ) ;
+		legend->SetHeader( "Crunch times", "C" ) ;
+		legend->SetBorderSize( 1 ) ;
+		
+		TF1 *xyline = new TF1( "y=x", "x", 0., static_cast<float>( maxCores+1 ) ) ;
+		xyline->SetLineColor( kBlack ) ;
+		xyline->SetLineStyle( 7 ) ;
+		xyline->SetLineWidth( 2 ) ;
+		xyline->Draw( "same" ) ;
+		
+		TText *t = new TText( 0.7, 0.72, "Perfect scaling" ) ;
+		t->SetNDC() ;
+		t->SetTextAlign(22);
+		t->SetTextColor(kGray+1);
+		t->SetTextFont(43);
+		t->SetTextSize(38);
+		t->SetTextAngle(45);
+		t->Draw();
+		
+
+	}
+	speedupVsCoreCanvas->GetFrame()->SetFillColor( 19 ) ;
+	if( save ) {
+		speedupVsCoreCanvas->SaveAs( (fname + "SpeedupVSNCores.pdf").c_str() ) ;
+	}
+}
