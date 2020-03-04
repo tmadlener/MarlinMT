d7ddd3344ae8e5185dd925382f01bd6fe4795ea7
Remi Ete <remi.ete@gmail.com>
2020-03-04 17:25:59 +0100

    Added new Configuration classes

---
+++ source/framework/include/marlin/Configuration.h
@@ -0,0 +1,315 @@
+#pragma once
+
+// -- std headers
+#include <string>
+#include <map>
+#include <vector>
+
+
+// -- marlin headers
+#include <marlin/Utils.h>
+#include <marlin/Exceptions.h>
+
+namespace marlin {
+  
+  class Configurable ;
+  
+  /**
+   *  @brief  ConfigSection class
+   *  Holds a set of parameters and subsection
+   */
+  class ConfigSection {
+  public:
+    using ConfigSectionMap = std::map<std::string, ConfigSection> ;
+    using ParameterMap = std::map<std::string, std::string> ;
+    
+  public:
+    // default copy and move
+    ConfigSection( const ConfigSection& ) = default ;
+    ConfigSection& operator=( const ConfigSection& ) = default ;
+    ConfigSection( ConfigSection&& ) = default ;
+    ConfigSection& operator=( ConfigSection&& ) = default ;
+    
+    /**
+     *  @brief  Constructor with name
+     * 
+     *  @param  name the section name
+     */
+    ConfigSection( const std::string &name ) ;
+    
+    /**
+     *  @brief  Get the section name
+     */
+    const std::string &name() const ;
+    
+    /**
+     *  @brief  Whether the section is empty (no parameter, no subsection)
+     */
+    bool empty() const ;
+    
+    /** @name Sections
+     */
+    ///@{ 
+    /**
+     *  @brief  Get a subsection by name. Throws if not found
+     * 
+     *  @param  n the subsection name
+     */
+    ConfigSection &section( const std::string &n ) ;
+    
+    /**
+     *  @brief  Get a subsection by name (const version). Throws if not found
+     * 
+     *  @param  n the subsection name
+     */
+    const ConfigSection &section( const std::string &n ) const ;
+    
+    /**
+     *  @brief  Create a new subsection. Throws if already exists
+     * 
+     *  @param  n the subsection name
+     */
+    ConfigSection &addSection( const std::string &n ) ;
+    
+    /**
+     *  @brief  Whether the subsection exists
+     * 
+     *  @param  n the subsection name
+     */
+    bool hasSection( const std::string &n ) const ;
+    
+    /**
+     *  @brief  Get the list of subsection names
+     */
+    std::vector<std::string> subsectionNames() const ;
+    ///@}
+    
+    /** @name Parameters
+     */
+    ///@{
+    /**
+     *  @brief  Set a parameter value. If the parameter exists it is replaced
+     * 
+     *  @param  n the parameter name
+     *  @param  val the parameter value
+     */
+    ConfigSection &setParameter( const std::string &n, const std::string &val ) ;
+    
+    /**
+     *  @brief  Get a parameter value as type T. 
+     *  Throw if the parameter doesn't exist
+     * 
+     *  @param  n the parameter name
+     */
+    template <typename T>
+    inline T parameter( const std::string &n ) const {
+      auto iter = _parameters.find( n ) ;
+      if( _parameters.end() == iter  ) {
+        MARLIN_THROW( "No parameter '" + n + "' in section '" + name() + "'" ) ;
+      }
+      return details::convert<T>::from_string( iter->second ) ;
+    }
+    
+    /**
+     *  @brief  Get a parameter value as type T. 
+     *  Return the default value if the parameter doesn't exist
+     *  
+     *  @param  n the parameter name
+     *  @param  defVal the defult value
+     */
+    template <typename T>
+    inline T parameter( const std::string &n, const T &defVal ) const {
+      auto iter = _parameters.find( n ) ;
+      if( _parameters.end() == iter  ) {
+        return defVal ;
+      }
+      return details::convert<T>::from_string( iter->second ) ;
+    }
+    
+    /**
+     *  @brief  Whether the parameter exists
+     * 
+     *  @param  n the parameter name
+     */
+    bool hasParameter( const std::string &n ) const ;
+    
+    /**
+     *  @brief  Get the list of parameter names
+     */
+    std::vector<std::string> parameterNames() const ;
+    ///@}
+    
+    /**
+     *  @brief  Load the parameters of the current section in the configurable object
+     * 
+     *  @param  cfg the configurable object
+     */
+    void loadParameters( Configurable &cfg ) const ;
+    
+  private:
+    /// The subsection map
+    ConfigSectionMap           _subsections {} ;
+    /// The parameter map
+    ParameterMap               _parameters {} ;
+    /// The section name
+    const std::string          _name ;
+  };
+  
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+  
+  /**
+   *  @brief  Configuration class.
+   *  Top level configuration class holding config sections and constant.
+   *  It is the main object to populate when reading the configuration from config files
+   */
+  class Configuration {
+  public:
+    using ConfigSectionMap = ConfigSection::ConfigSectionMap ;
+    using ConstantsMap = std::map<std::string, std::string> ;
+    
+  public:
+    // Default constructors, destructors, copy, move
+    Configuration() = default ;
+    ~Configuration() = default ;
+    Configuration( const Configuration & ) = default ;
+    Configuration( Configuration && ) = default ;
+    Configuration& operator=( const Configuration & ) = default ;
+    Configuration& operator=( Configuration && ) = default ;
+    
+    /** @name Constants
+     */
+    ///@{
+    /**
+     *  @brief  Get a constant value as type T. Throw if not found
+     * 
+     *  @param  cn the constant name
+     */
+    template <typename T>
+    T constantAs( const std::string &cn ) const {
+      auto iter = _constants.find( cn ) ;
+      if( iter == _constants.end() ) {
+        MARLIN_THROW( "No constant '" + cn +  "' in configuration" ) ;
+      }
+      return details::convert<T>::from_string( iter->second ) ;
+    }
+    
+    /**
+     *  @brief  Get a constant value as type T. 
+     *  Return the default value if not found
+     * 
+     *  @param  cn the constant name
+     *  @param  defVal the default value
+     */
+    template <typename T>
+    T constantAs( const std::string &cn, const T &defVal ) const {
+      auto iter = _constants.find( cn ) ;
+      if( iter == _constants.end() ) {
+        return defVal ;
+      }
+      return details::convert<T>::from_string( iter->second ) ;
+    }
+    
+    /**
+     *  @brief  Add a constant. The value must be convertible to/from string.
+     *  Throw if already the constant already exists
+     *   
+     *  @param  cn the constant name
+     *  @param  val the constant value
+     */
+    template <typename T>
+    Configuration &addConstant( const std::string &cn, const T &val ) {
+      auto iter = _constants.find( cn ) ;
+      if( iter != _constants.end() ) {
+        MARLIN_THROW( "Constant '" + cn +  "' already present in configuration" ) ;
+      }
+      _constants.emplace( cn, details::convert<T>::to_string( val ) ) ;
+      return *this ;
+    }
+    
+    /**
+     *  @brief  Whetehr the constant is registered in the configuration
+     */
+    bool hasConstant( const std::string &cn ) const ;
+    
+    /**
+     *  @brief  Get the constants map
+     */
+    const ConstantsMap &constants() const ;
+    ///@}
+    
+    /** @name Sections
+     */
+    ///@{
+    /**
+     *  @brief  Create a new section by name. Throw if already exists 
+     *  
+     *  @param  sn the new section name
+     */
+    ConfigSection &createSection( const std::string &sn ) ;
+    
+    /**
+     *  @brief  Get a section by name. Throw if doesn't exists 
+     *  
+     *  @param  sn the section name
+     */
+    ConfigSection &section( const std::string &sn ) ;
+    
+    /**
+     *  @brief  Get a section by name. Throw if doesn't exists 
+     *  
+     *  @param  sn the section name
+     */
+    const ConfigSection &section( const std::string &sn ) const ;
+    ///@}    
+
+    /** @name Helper methods
+     */
+    ///@{
+    /**
+     *  @brief  Replace all occurences of ${constant_name} in the input string
+     *  where "constant_name" must match a registered constant name
+     *  
+     *  @param  str the input string to modify
+     */
+    void replaceConstants( std::string& str ) const ;
+    ///@}
+    
+  private:
+    /// The top level configuration sections
+    ConfigSectionMap           _sections {} ;
+    /// The constants map
+    ConstantsMap               _constants {} ;
+  };
+  
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+  
+  /**
+   *  @brief  ConfigParser base class
+   *  Interface for reading and writing configuration in different formats
+   */
+  class ConfigParser {
+  public:
+    using ReplacementParametersMap = std::map<std::string,std::map<std::string,std::string>> ;
+    
+    /// Default destructor
+    virtual ~ConfigParser() = default ;
+
+    /**
+     *  @brief  Read the configuration and populate the configuration object.
+     *  Parameters can be used in the parsing process to substitute values on the fly
+     *  
+     *  @param  cfg the configuration object to populate
+     */
+    virtual void read( Configuration &cfg, const ReplacementParametersMap &params = {} ) = 0 ;
+    
+    /**
+     *  @brief  Write the configuration object
+     * 
+     *  @param  cfg the configuration object to write
+     */
+    virtual void write( const Configuration &cfg ) = 0 ;
+  };
+    
+}
---
+++ source/framework/src/Configuration.cc
@@ -0,0 +1,157 @@
+// -- marlin headers
+#include <marlin/Configuration.h>
+#include <marlin/Parameter.h>
+
+// -- std headers
+#include <algorithm>
+
+namespace marlin {
+  
+  ConfigSection::ConfigSection( const std::string &name ) : 
+    _name(name) {
+    /* nop */
+  }
+  
+  //--------------------------------------------------------------------------
+
+  const std::string &ConfigSection::name() const {
+    return _name ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  bool ConfigSection::empty() const {
+    return (_subsections.empty() && _parameters.empty() ) ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  ConfigSection &ConfigSection::section( const std::string &n ) {
+    auto iter = _subsections.find( n ) ;
+    if( _subsections.end() == iter ) {
+      MARLIN_THROW( "No sub-section '" + n + "' in section '" + name() + "'" ) ;
+    }
+    return iter->second ;
+  }
+  
+  //--------------------------------------------------------------------------
+
+  const ConfigSection &ConfigSection::section( const std::string &n ) const {
+    auto iter = _subsections.find( n ) ;
+    if( _subsections.end() == iter ) {
+      MARLIN_THROW( "No sub-section '" + n + "' in section '" + name() + "'" ) ;
+    }
+    return iter->second ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  ConfigSection &ConfigSection::addSection( const std::string &n ) {
+    auto iter = _subsections.find( n ) ;
+    if( _subsections.end() != iter ) {
+      MARLIN_THROW( "Sub-section '" + n + "' already present in section '" + name() + "'" ) ;
+    }
+    return _subsections.emplace( n, ConfigSection{n} ).first->second ;
+  }
+  
+  //--------------------------------------------------------------------------
+
+  bool ConfigSection::hasSection( const std::string &n ) const {
+    return (_subsections.end() != _subsections.find( n )) ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  std::vector<std::string> ConfigSection::subsectionNames() const {
+    std::vector<std::string> names( _subsections.size() ) ;
+    std::transform( _subsections.begin(), _subsections.end(), names.begin(), [] (const auto &iter) { return iter.first ; } ) ;
+    return names ;
+  }
+  
+  //--------------------------------------------------------------------------
+
+  ConfigSection &ConfigSection::setParameter( const std::string &n, const std::string &val ) {
+    _parameters.insert_or_assign( n, val ) ;
+    return *this ;
+  }
+  
+  //--------------------------------------------------------------------------
+
+  bool ConfigSection::hasParameter( const std::string &n ) const {
+    return (_parameters.end() != _parameters.find( n )) ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  std::vector<std::string> ConfigSection::parameterNames() const {
+    std::vector<std::string> names( _parameters.size() ) ;
+    std::transform( _parameters.begin(), _parameters.end(), names.begin(), [] (const auto &iter) { return iter.first ; } ) ;
+    return names ;
+  }
+  
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+  
+  bool Configuration::hasConstant( const std::string &cn ) const {
+    return ( _constants.end() != _constants.find( cn ) ) ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  const Configuration::ConstantsMap &Configuration::constants() const {
+    return _constants ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  ConfigSection &Configuration::createSection( const std::string &sn ) {
+    auto iter = _sections.find( sn ) ;
+    if( _sections.end() != iter ) {
+      MARLIN_THROW( "Section '" + sn + "' already exists" ) ;
+    }
+    return _sections.emplace( sn, ConfigSection{sn} ).first->second ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  ConfigSection &Configuration::section( const std::string &sn ) {
+    auto iter = _sections.find( sn ) ;
+    if( _sections.end() == iter ) {
+      MARLIN_THROW( "Section '" + sn + "' doesn't exists" ) ;
+    }
+    return iter->second ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  const ConfigSection &Configuration::section( const std::string &sn ) const {
+    auto iter = _sections.find( sn ) ;
+    if( _sections.end() == iter ) {
+      MARLIN_THROW( "Section '" + sn + "' doesn't exists" ) ;
+    }
+    return iter->second ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  void Configuration::replaceConstants( std::string& str ) const {
+    size_t pos = str.find("${") ;
+    while( pos != std::string::npos ) {
+      size_t pos2 = str.find_first_of( "}", pos+2 ) ;
+      if( pos2 == std::string::npos ) {
+        MARLIN_THROW_T( ParseException, "Couldn't parse constant value in string '" + str + "'. It seems you have an open bracket '${' but it is not closed" ) ;
+      }
+      const std::string key( str.substr( pos+2 , pos2-pos-2 )) ;
+      auto iter = _constants.find( key ) ;
+      const std::string replacementValue( iter != _constants.end() ? iter->second : "" ) ;
+      if( replacementValue.empty() ) {
+        MARLIN_THROW_T( ParseException, "Constant '" + key + "' doesn't exists !" ) ;
+      }
+      str.replace( pos , (pos2+1-pos) , replacementValue ) ;
+      pos2 = pos + replacementValue.size() ; // new end position after replace
+      pos = str.find("${", pos2) ; // find next possible key to replace
+    }
+  }
+  
+  
+}
