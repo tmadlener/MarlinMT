9b052622907f441ebae373603d500b5f0d5376f2
Remi Ete <remi.ete@gmail.com>
2020-03-12 18:42:12 +0100

    Added class CmdLineParser

---
+++ source/framework/include/marlin/CmdLineParser.h
@@ -0,0 +1,49 @@
+#pragma once
+
+// -- std headers
+#include <map>
+#include <string>
+#include <vector>
+
+namespace marlin {
+
+  /**
+   *  @brief  CmdLineParser class
+   *  Does the main command line parsing for MarlinMT
+   */
+  class CmdLineParser {
+  public:
+    using AdditionalArgs = std::map<std::string, std::string> ;
+    
+    /// ParseResult struct. The result of the command line parsing. 
+    struct ParseResult {
+      /// The program name
+      std::string                             _programName {} ;
+      /// All command line arguments except the program name (argv[0])
+      std::vector<std::string>                _arguments {} ;
+      /// Additional command line arguments
+      AdditionalArgs                          _additionalArgs {} ;
+      /// The config argument
+      std::string                             _config {} ;
+      /// The number of threads
+      unsigned int                            _nthreads {1} ;
+      /// Whether to dump an example configuration and exit
+      bool                                    _dumpExample {false} ;
+    };
+    
+  public:
+    /// Default constructor
+    CmdLineParser() = default ;
+    /// Default destructor
+    ~CmdLineParser() = default ;
+    
+    /**
+     *  @brief  Parse the command line
+     * 
+     *  @param  argc the number of arguments
+     *  @param  argv the full argument list
+     */
+    [[nodiscard]] ParseResult parse( int argc, char**argv ) ;
+  };
+  
+}
---
+++ source/framework/src/CmdLineParser.cc
@@ -0,0 +1,78 @@
+
+// -- marlin headers
+#include <marlin/CmdLineParser.h>
+#include <marlin/MarlinConfig.h>
+#include <marlin/Exceptions.h>
+#include <marlin/Utils.h>
+
+// -- TCLAP headers
+#include <tclap/CmdLine.h>
+#include <tclap/ValueArg.h>
+
+namespace marlin {
+  
+  CmdLineParser::ParseResult CmdLineParser::parse( int argc, char**argv ) {
+    ParseResult parseResult {} ;
+    parseResult._arguments.reserve(argc) ;
+    // Create command line object
+    TCLAP::CmdLine cmdLine( "MarlinMT", ' ', MARLIN_RELEASE , true ) ;
+    // Config argument
+    TCLAP::ValueArg<std::string> configArg( "c", "config", "The input configuration settings (plugin:descriptor)", true, "", "string" ) ;
+    // Dump example config argument
+    TCLAP::ValueArg<std::string> exampleArg( "x", "example", "Dump an example configuration (plugin:descriptor)", true, "", "string" ) ;
+    cmdLine.xorAdd( configArg, exampleArg ) ;
+    // number of cores
+    TCLAP::ValueArg<std::string> nthreadsArg( "j", "nthreads", "The number of worker threads (only in multi-threaded mode). 'auto' will use all available cores", false, "auto", "unsigned int / string" ) ;
+    cmdLine.add( nthreadsArg ) ;
+    // parse additional args
+    std::vector<std::string> parserArgs{} ;
+    parserArgs.reserve(argc) ; 
+    parserArgs.push_back( argv[0] ) ; // for the program name
+    for(int i=1 ; i<argc ; i++) {
+      parseResult._arguments.push_back( argv[i] ) ;
+    }
+    auto &args = cmdLine.getArgList() ;
+    // analyze the arguments.
+    for( auto &arg : parseResult._arguments ) {
+      if( arg.substr( 0, 2 ) == "--" ) {
+        auto argName = arg.substr( 2 ) ;
+        auto iter = std::find_if( args.begin(), args.end(), [&](auto val){
+          return (val->getName() == argName) ;
+        }) ;
+        // Not an additional arg. Add it for later parsing
+        if( args.end() != iter ) {
+          parserArgs.push_back( arg ) ;
+          continue ;
+        }
+        // Parse the additional argument
+        auto tokens = details::split_string<std::string>( argName, "=" ) ;
+        if( 2 != tokens.size() ) {
+          MARLIN_THROW( "Additional argument '" + arg + "' with wrong syntax. Expected '--name=value' format" ) ;
+        }
+        auto res = parseResult._additionalArgs.insert( { tokens[0], tokens[1] } ) ;
+        if( not res.second ) {
+          MARLIN_THROW( "Additional argument '" + arg + "' set twice on command line" ) ;
+        }
+      }
+      else {
+        parserArgs.push_back( arg ) ;
+      }
+    }
+    // parse the command line
+    cmdLine.parse( parserArgs ) ;
+    // extract command line values
+    parseResult._programName = cmdLine.getProgramName() ;
+    if( configArg.isSet() ) {
+      parseResult._config = configArg.getValue() ;
+      parseResult._dumpExample = false ;
+    }
+    else {
+      parseResult._config = exampleArg.getValue() ;
+      parseResult._dumpExample = true ;
+    }
+    parseResult._nthreads = details::nthreads( nthreadsArg.getValue() ) ;
+    return parseResult ;
+  }
+  
+}
+
