9fe08ac8a7b9536eff83c0595372202f4a8b7b37
Remi Ete <remi.ete@gmail.com>
2020-03-19 17:34:34 +0100

    Use new interface for MarlinMT application. Removed Marlin executable. Use case contained in MarlinMT -j1

--- source/framework/main/MarlinMT.cc
+++ source/framework/main/MarlinMT.cc
@@ -1,48 +1,41 @@
 // -- marlin headers
 #include <marlin/Application.h>
-#include <marlin/concurrency/PEPScheduler.h>
 #include <marlin/PluginManager.h>
+#include <marlin/Utils.h>
 
 using namespace marlin ;
 
-#define MARLIN_MAIN_LOG( MLEVEL , message ) \
-  if ( nullptr != application ) { \
-    application->logger()->log<MLEVEL>() << message ; \
-  } \
-  else { \
-    streamlog_out(MLEVEL) << message ; \
-  }
-
 int main( int argc, char **argv ) {
-  
-  std::shared_ptr<Application> application {nullptr} ;
   
   // load plugins first
   auto &mgr = PluginManager::instance() ;
-  mgr.logger()->setLevel<MESSAGE>();
-  if ( not mgr.loadLibraries() ) {
-    throw Exception( "Couldn't load shared libraries from MARLIN_DLL !" ) ;
-  }
+  mgr.logger()->setLevel<MESSAGE>() ;
+  auto libraries = details::split_string<std::string>( 
+    details::getenv<std::string>( "MARLIN_DLL", "" ), 
+    ":" 
+  );
+  mgr.loadLibraries( libraries ) ;
+  
   // configure and run application
-  try {
-    application = std::make_shared<Application>() ;    
-    application->setScheduler( std::make_shared<concurrency::PEPScheduler>() ) ;
-    application->init( argc, argv ) ;
-    application->run() ;
+  Application application ;
+  auto logger = application.logger() ;
+  try {    
+    application.init( argc, argv ) ;
+    application.run() ;
   }
   catch ( marlin::Exception &e ) {
-    MARLIN_MAIN_LOG( ERROR, "Marlin main, caught Marlin exception " << e.what() << std::endl )
-    MARLIN_MAIN_LOG( ERROR, "Exiting with status 1" << std::endl )
+    logger->log<ERROR>() << "MarlinMT main, caught Marlin exception " << e.what() << std::endl ;
+    logger->log<ERROR>() << "Exiting with status 1" << std::endl ;
     return 1 ;
   }
   catch ( std::exception &e ) {
-    MARLIN_MAIN_LOG( ERROR, "Marlin main, caught std::exception " << e.what() << std::endl )
-    MARLIN_MAIN_LOG( ERROR, "Exiting with status 1" << std::endl )
+    logger->log<ERROR>() << "MarlinMT main, caught std::exception " << e.what() << std::endl ;
+    logger->log<ERROR>() << "Exiting with status 1" << std::endl ;
     return 1 ;
   }
   catch ( ... ) {
-    MARLIN_MAIN_LOG( ERROR, "Marlin main, caught unknown exception" << std::endl )
-    MARLIN_MAIN_LOG( ERROR, "Exiting with status 2" << std::endl )
+    logger->log<ERROR>() << "MarlinMT main, caught unknown exception" << std::endl ;
+    logger->log<ERROR>() << "Exiting with status 2" << std::endl ;
     return 2 ;
   }
 
