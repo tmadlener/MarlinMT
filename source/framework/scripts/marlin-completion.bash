2d2f06eddfd3ab40337c45cbff0a873223783a30
Remi Ete <remi.ete@gmail.com>
2020-03-23 16:23:10 +0100

    Updated shell completion script

--- source/framework/scripts/marlin-completion.bash
+++ source/framework/scripts/marlin-completion.bash
@@ -4,9 +4,12 @@ marlin_completion() {
   if ! [ -x "$(command -v MarlinShellCompletion)" ]; then
     return
   fi
-  xmlfile=""
+  config=""
   current_id=0
   comp_marlin=""
+  # Loop over current command lien arguments
+  # 1) Skip the executable name argument
+  # 2) Skip the last argument which is the one we want to complete
   for arg in $COMP_LINE; do
     # echo "Current id is $current_id"
     if [ $current_id == 0 ]; then
@@ -14,31 +17,18 @@ marlin_completion() {
       ((current_id++))
       continue
     fi
-    if [ "${arg: -4}" == ".xml" ] && [[ $arg != --* ]]; then
-      xmlfile=$arg
-      # echo "Arg no $current_id is XML file"
-      ((current_id++))
-      continue
-    fi
-    if [ "$arg" != "${COMP_WORDS[${COMP_CWORD}]}" ] && [[ $arg == --* ]]; then
+    if [ "$arg" != "${COMP_WORDS[${COMP_CWORD}]}" ]; then
       comp_marlin="$comp_marlin $arg"
     fi
     ((current_id++))
   done
-  if [ -z "$xmlfile" ]; then
-    return
-  fi
-  # echo "XML file is $xmlfile"
-  opts=`MarlinShellCompletion $xmlfile $comp_marlin`
+  opts=`MarlinShellCompletion $comp_marlin`
   ret=$?
   if ! [ $ret == 0 ]; then
-    echo ""
-    echo "<invalid steering file>"
     return 
   fi
   opts=`echo $opts | tr '\n' ' '`  
   COMPREPLY=($(compgen -W '$opts' -- "${COMP_WORDS[${COMP_CWORD}]}"))
 }
 
-complete -o default -o nospace -F marlin_completion Marlin
 complete -o default -o nospace -F marlin_completion MarlinMT
