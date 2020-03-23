2035366a5f27f72165f89d506ed373b4d742336a
Remi Ete <remi.ete@gmail.com>
2020-03-23 16:12:03 +0100

    Added special Output to command line to enable verbosity level tweaking

--- source/framework/src/CmdLineParser.cc
+++ source/framework/src/CmdLineParser.cc
@@ -3,6 +3,7 @@
 #include <marlin/CmdLineParser.h>
 #include <marlin/MarlinConfig.h>
 #include <marlin/Exceptions.h>
+#include <marlin/Logging.h>
 #include <marlin/Utils.h>
 
 // -- TCLAP headers
@@ -10,6 +11,61 @@
 #include <tclap/ValueArg.h>
 
 namespace marlin {
+  
+  class Output : public TCLAP::StdOutput {
+  	public:
+  		void failure(TCLAP::CmdLineInterface& c, TCLAP::ArgException& e) override { 
+        std::string progName = c.getProgramName();
+        auto &logger = Logging::globalLogger() ;
+      	logger.log<ERROR>() << "PARSE ERROR: " << e.argId() << std::endl
+      		      << "             " << e.error() << std::endl << std::endl;
+      	if ( c.hasHelpAndVersion() )
+      		{
+            
+      			logger.log<ERROR>() << "Brief USAGE: " << std::endl;
+            {
+              auto stream = logger.log<ERROR>() ;
+        			_shortUsage( c, stream );	              
+            }
+
+
+      			logger.log<ERROR>() << std::endl << "For complete USAGE and HELP type: " 
+      					  << std::endl << "   " << progName << " "
+      					  << TCLAP::Arg::nameStartString() << "help"
+      					  << std::endl << std::endl;
+      		}
+      	else
+      		usage(c);
+
+      	throw TCLAP::ExitException(1);
+  		}
+
+  		void usage(TCLAP::CmdLineInterface& c) override {
+        auto &logger = Logging::globalLogger() ;
+        logger.log<MESSAGE>() << std::endl << "USAGE: " << std::endl << std::endl; 
+        {
+          auto stream = logger.log<MESSAGE>() ;
+        	_shortUsage( c, stream );          
+        }
+      	logger.log<MESSAGE>() << std::endl << std::endl << "Where: " << std::endl << std::endl;
+        {
+          auto stream = logger.log<MESSAGE>() ;
+      	  _longUsage( c, stream );
+        }
+      	logger.log<MESSAGE>() << std::endl; 
+  		}
+
+  		void version(TCLAP::CmdLineInterface& c) override {
+        std::string progName = c.getProgramName();
+        std::string xversion = c.getVersion();
+        auto &logger = Logging::globalLogger() ;
+        logger.log<MESSAGE>() << std::endl << progName << "  version: " 
+              << xversion << std::endl << std::endl;
+  		}
+  };
+  
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
   
   void CmdLineParser::setOptionalArgs( bool opt ) {
     _optionalArgs = opt ;
@@ -31,6 +87,8 @@ namespace marlin {
     parseResult._arguments.reserve(argc) ;
     // Create command line object
     TCLAP::CmdLine cmdLine( "MarlinMT", ' ', MARLIN_RELEASE , true ) ;
+    Output output ;
+    cmdLine.setOutput( &output ) ;
     // Config argument
     TCLAP::ValueArg<std::string> configArg( "c", "config", "The input configuration settings (plugin:descriptor)", (!_optionalArgs), "", "string" ) ;
     // Dump example config argument
